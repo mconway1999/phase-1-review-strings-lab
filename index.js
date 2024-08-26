8c3cbbb68baba7a44d21de285dca2d1eba07ecf9
Madeline Conway <conway.maddy@yahoo.com>
2024-08-26 13:32:57 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper!';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+typeof "const currentUser = 'Grace Hopper';"
+"const currentUser = 'Grace Hopper';".length;
+currentUser;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+currentUser.length;
+currentUser.slice(0, 1);
+const shortGreeting = `Welcome, to Flatbook ${currentUser.slice(0, 1)}!`;
+
